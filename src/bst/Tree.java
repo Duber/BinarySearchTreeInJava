74e695f30fc5b6bd07d1f3a4bdcdc43c2f07c53f
duber <miguel@miguelcruz.net>
2015-07-21 20:18:49 +0200

    Second and Third Tests adding higher and lower values

--- src/bst/Tree.java
+++ src/bst/Tree.java
@@ -5,6 +5,8 @@ package bst;
  */
 public class Tree {
     private int _value;
+    private Tree _leftSubTree;
+    private Tree _rightSubTree;
 
     public Tree(int value) {
         _value = value;
@@ -12,5 +14,29 @@ public class Tree {
 
     public int GetValue() {
         return _value;
+    }
+
+    public void AddValue(int value) {
+        if (value > _value){
+            if (_rightSubTree == null){
+                _rightSubTree = new Tree(value);
+            } else {
+                _rightSubTree.AddValue(value);
+            }
+        } else if (value < _value){
+            if (_leftSubTree == null){
+                _leftSubTree = new Tree(value);
+            } else {
+                _leftSubTree.AddValue(value);
+            }
+        }
+    }
+
+    public Tree GetLeftSubTree() {
+        return _leftSubTree;
+    }
+
+    public Tree GetRightSubTree(){
+        return _rightSubTree;
     }
 }
--- test/bst/TreeShould.java
+++ test/bst/TreeShould.java
@@ -9,11 +9,33 @@ import org.junit.Test;
 public class TreeShould {
 
     @Test
-    public void CreateATreeWithASingleValue(){
+    public void CreateATreeWithAValue(){
         int value = 1;
         Tree tree = new Tree(value);
 
         Assert.assertEquals(value, tree.GetValue());
+    }
+
+    @Test
+    public void AddAHigherValueToATree(){
+        int firstValue = 1;
+        int secondValue = 2;
+        Tree tree = new Tree(firstValue);
+
+        tree.AddValue(secondValue);
+
+        Assert.assertEquals(secondValue, tree.GetRightSubTree().GetValue());
+    }
+
+    @Test
+    public void AddALowerValueToATree(){
+        int firstValue = 2;
+        int secondValue = 1;
+        Tree tree = new Tree(firstValue);
+
+        tree.AddValue(secondValue);
+
+        Assert.assertEquals(secondValue, tree.GetLeftSubTree().GetValue());
     }
 
 }
