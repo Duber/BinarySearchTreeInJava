94e9711124956a32cb125da8cf98ff6a107c0ff4
duber <miguel@miguelcruz.net>
2015-07-21 19:55:02 +0200

    Initial commit

---
+++ src/bst/Tree.java
@@ -0,0 +1,7 @@
+package bst;
+
+/**
+ * Created by duber on 21/07/15.
+ */
+public class Tree {
+}
---
+++ test/bst/TreeShould.java
@@ -0,0 +1,16 @@
+package bst;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by duber on 21/07/15.
+ */
+public class TreeShould {
+
+    @Test
+    public void CreateAnEmptyTree(){
+        Assert.assertTrue(true);
+    }
+
+}
